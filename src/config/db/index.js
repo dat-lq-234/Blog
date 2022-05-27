b45b9c6d089e03baa7081441c9e796e573b49a3c
dat-lq <lq.dat.234@gmail.com>
2022-05-27 20:12:24 +0700

    fix bug

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function connect(){
     try {
-        await mongoose.connect('mongodb+srv://dat-lq:datlq234@cluster0.oy3st.mongodb.net/?retryWrites=true&w=majority');
+        await mongoose.connect(process.env.MONGODB_URI);
         // useCreateIndex: true,
         console.log("connect successfully!")
     } catch (error) {
